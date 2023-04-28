de2b4e0049ceb0d65c2eb3ea1489f1fe919da15f
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-04-28 08:19:51 -0300

    Criando o MVC

--- Controllers/VeiculosController.cs
+++ Controllers/VeiculosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaOficinas.Models;
 
 namespace SistemaOficinas.Controllers
 {
@@ -7,6 +8,18 @@ namespace SistemaOficinas.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CadastroVeiculos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastroVeiculos(VeiculosModel veiculosModel)
+        {
+            return View(veiculosModel);
         }
     }
 }
