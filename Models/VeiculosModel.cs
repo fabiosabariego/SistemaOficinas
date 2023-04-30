e9eb1631ab953b2afa13db7730e0dcbfc7e32870
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-04-30 19:03:50 -0300

    Inserido Cadastro de Veiculos

--- Models/VeiculosModel.cs
+++ Models/VeiculosModel.cs
@@ -1,14 +1,19 @@
-﻿namespace SistemaOficinas.Models
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace SistemaOficinas.Models
 {
     public class VeiculosModel
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
-        public string? Marca { get; set; }
+        public string? Fabricante { get; set; }
         public string? Modelo { get; set; }
+        public string? Cor { get; set; }
         public int Ano { get; set; }
         public string? Placa { get; set; }
         public int Km { get; set; }
         public string? Obs { get; set; }
+
     }
+
+    
 }
